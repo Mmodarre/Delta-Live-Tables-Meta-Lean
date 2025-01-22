6bda64fb2bd016b922a5a1bd912fe49764b4e447
mmodarre <modarressi.mehdi@gmail.com>
2025-01-22 14:13:13 +1100

    Add materiazedView parameter to populate_silver function for enhanced data processing

--- src/populate_silver_job.py
+++ src/populate_silver_job.py
@@ -39,6 +39,7 @@ selectExp = ["cast(customers_id as String) as customer_id","address","email","fi
 whereClause = None
 partitionColumns = None
 cdcApplyChanges = '{"apply_as_deletes": "operation = \'DELETE\'","track_history_except_column_list": ["file_path","processing_time"], "except_column_list": ["operation"], "keys": ["customer_id"], "scd_type": "2", "sequence_by": "operation_date"}'
+materiazedView = None
 dataQualityExpectations = '{"expect_or_drop": {"valid_customer_id": "customers_id IS NOT NULL"}}'
 quarantineTargetDetails = None
 quarantineTableProperties = None
@@ -49,4 +50,4 @@ updatedBy = spark.range(1).select(current_user()).head()[0]
 SILVER_MD_TABLE = BRONZE_MD_TABLE = f"{catalog}{env}.{db}.silver_dataflowspec_table" # type: ignore
 
 
-populate_silver(SILVER_MD_TABLE,dataFlowId, dataFlowGroup, sourceFormat, sourceDetails, readerConfigOptions, targetFormat, targetDetails, tableProperties,selectExp,whereClause,partitionColumns, cdcApplyChanges, dataQualityExpectations,createDate, createdBy,updateDate, updatedBy,spark)
+populate_silver(SILVER_MD_TABLE,dataFlowId, dataFlowGroup, sourceFormat, sourceDetails, readerConfigOptions, targetFormat, targetDetails, tableProperties,selectExp,whereClause,partitionColumns, cdcApplyChanges, materiazedView, dataQualityExpectations,createDate, createdBy,updateDate, updatedBy,spark)
